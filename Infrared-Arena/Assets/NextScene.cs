33465b7668715e22de4b8106b903388fc2e360f7
lphanvilay <linda.phanvilay@mavs.uta.edu>
2019-11-06 15:24:53 -0600

    Added buttons
    Buttons for going to between scenes

---
+++ Infrared-Arena/Assets/NextScene.cs
@@ -0,0 +1,13 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class NextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public void OnMouseDown(string screen)
+	{
+		SceneManager.LoadScene(screen);
+	}
+}
