0699b3e5aae4f41b784f61d2943ac322218f7e6c
lphanvilay <linda.phanvilay@mavs.uta.edu>
2019-11-10 16:57:32 -0600

    Added more screens - scenes
    Fixed issue I had with buttons not being in view when running

---
+++ Infrared-Arena/Assets/InputField.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputField : MonoBehaviour
+{
+	public InputField inputText;
+	string nickname;
+
+	void Start(){
+		nickname = PlayerPrefs.GetString("nickname");
+		inputText.text = nickname;
+	}
+
+	public void SaveThis(){
+		nickname = inputText;
+		PlayerPrefs.SetString("nickname", nickname);
+	}
+}
