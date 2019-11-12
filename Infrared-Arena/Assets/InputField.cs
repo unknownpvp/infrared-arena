0834d4a187edf5ea2682ca10fa4351bc994f7359
lphanvilay <linda.phanvilay@mavs.uta.edu>
2019-11-11 23:13:18 -0600

    Added input field to profile page

--- Infrared-Arena/Assets/InputField.cs
+++ Infrared-Arena/Assets/InputField.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 
 public class InputField : MonoBehaviour
 {
-	public InputField inputText;
+	public UnityEngine.UI.InputField inputText;
 	string nickname;
 
 	void Start(){
-		nickname = PlayerPrefs.GetString("nickname");
-		inputText.text = nickname;
+		//nickname = gameObject.GetComponent<InputField>();//PlayerPrefs.GetString("nickname");
+		//inputText.text = nickname;
+		inputText = gameObject.GetComponent <UnityEngine.UI.InputField>();
 	}
 
 	public void SaveThis(){
-		nickname = inputText;
+		nickname = inputText.text;
 		PlayerPrefs.SetString("nickname", nickname);
+		Debug.Log("Player Name is: " + inputText.text);
+    //	Settings.playername = inputText.text;
 	}
 }
