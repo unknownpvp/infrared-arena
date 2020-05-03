1fddaf6a1efa7f7b94a993aea6a84fb35d4bbeae
JM-Yacho <jmyach08@gmail.com>
2020-05-03 11:04:27 -0500

    Revert "2d map"
    This reverts commit 30b1a93b223ebeb0797fea7bca605701f24c861a.

---
+++ Infrared-Arena/Assets/InputField.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputField : MonoBehaviour
+{
+	public UnityEngine.UI.InputField inputText;
+	string nickname;
+
+	void Start(){
+		//nickname = gameObject.GetComponent<InputField>();//PlayerPrefs.GetString("nickname");
+		//inputText.text = nickname;
+		inputText = gameObject.GetComponent <UnityEngine.UI.InputField>();
+	}
+
+	public void SaveThis(){
+		nickname = inputText.text;
+		PlayerPrefs.SetString("nickname", nickname);
+		Debug.Log("Player Name is: " + inputText.text);
+    //	Settings.playername = inputText.text;
+	}
+}
