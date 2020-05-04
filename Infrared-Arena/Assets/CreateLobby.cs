85d3afb143b53f69d49ce4d297ef24e2f6216a2f
lphanvilay <43015026+lphanvilay@users.noreply.github.com>
2020-05-04 03:29:54 -0500

    UI changes
    add google services json, add about and help pop-up, change homescreen and create lobby

---
+++ Infrared-Arena/Assets/CreateLobby.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CreateLobby : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+    string screen;
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "Player" + theName;
+        SceneManager.LoadScene(screen);
+    }
+
+}
---
+++ Infrared-Arena/Assets/HelpMenu.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpMenu : MonoBehaviour
+{
+    public static bool menuOpen = false;
+
+    public GameObject helpMenuUI;
+
+    public static bool aboutOpen = false;
+    public GameObject aboutUI;
+    private string openedObject;
+
+    //private void Update()
+    //{
+
+    //    if (Input.GetKeyDown(KeyCode.Escape))
+    //    {
+    //        if (menuOpen)
+    //        {
+    //            ResumeHelp();
+    //        }
+    //        else
+    //        {
+    //            PauseHelp();
+    //        }
+    //    }
+    //    if ()
+    //    {
+
+    //    }
+    //}
+    private void Update()
+    {
+            if (aboutOpen)
+            {
+                aboutUI.SetActive(true);
+                aboutOpen = true;
+            }
+            else
+            {
+                aboutUI.SetActive(false);
+                aboutOpen = false;
+            }
+
+            if (menuOpen) {
+                helpMenuUI.SetActive(true);
+                menuOpen = true;
+            }
+            else
+            {
+                helpMenuUI.SetActive(false);
+                menuOpen = false;
+            }
+    }
+    public void OnMouseDown(string openBox)
+    {
+        openedObject = openBox;
+        if (openBox == "about")
+        {
+            aboutOpen = true;
+        }
+        else if (openBox == "help")
+        {
+            menuOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        if (openedObject == "about")
+        {
+            aboutUI.SetActive(false);
+            aboutOpen = false;
+        }
+        else if (openedObject == "help")
+        {
+            helpMenuUI.SetActive(false);
+            menuOpen = false;
+        }
+    }
+}
