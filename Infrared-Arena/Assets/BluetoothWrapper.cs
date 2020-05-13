9f9b66bccf0b4874b38c23826d3584c52943c599
Justine Batongmalaki <jbt296@yahoo.com>
2020-05-13 13:05:51 -0500

    added files
    added danny's files

---
+++ Infrared-Arena/Assets/BluetoothWrapper.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BluetoothWrapper : MonoBehaviour
+{
+
+    AndroidJavaClass cls_UnityPlayer;
+    AndroidJavaObject obj_Activity;
+
+    public void ConnectBlaster()
+    {
+        cls_UnityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        obj_Activity = cls_UnityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+        obj_Activity.Call("connectWeapon");
+    }
+
+    void update()
+    {
+
+    }
+}
