46ca527f103065bc716d1141299612ef796bd43e
JM-Yacho <jmyach08@gmail.com>
2020-05-13 11:40:01 -0500

    Added Assets\LocationLog.txt

--- Infrared-Arena/Assets/Mapbox/Examples/Scripts/ImmediatePositionWithLocationProvider.cs
+++ Infrared-Arena/Assets/Mapbox/Examples/Scripts/ImmediatePositionWithLocationProvider.cs
@@ -3,6 +3,8 @@
 	using Mapbox.Unity.Location;
 	using Mapbox.Unity.Map;
 	using UnityEngine;
+	using System.IO;
+	using System.Text;
 
 	public class ImmediatePositionWithLocationProvider : MonoBehaviour
 	{
@@ -25,9 +27,19 @@
 
 		Vector3 _targetPosition;
 
+		string path;
+
 		void Start()
 		{
 			LocationProviderFactory.Instance.mapManager.OnInitialized += () => _isInitialized = true;
+
+			path = Application.dataPath + "/LocationLog.txt";
+			Debug.Log(path);
+
+			if(!File.Exists(path))
+			{
+				File.WriteAllText(path, "Begin log");
+			}
 		}
 
 		void LateUpdate()
@@ -38,6 +50,8 @@
 			
 				transform.localPosition = map.GeoToWorldPosition(LocationProvider.CurrentLocation.LatitudeLongitude, true);
 				map.UpdateMap(LocationProvider.CurrentLocation.LatitudeLongitude, map.Zoom);
+				string locationString = LocationProvider.CurrentLocation.LatitudeLongitude.x + "," + LocationProvider.CurrentLocation.LatitudeLongitude.y;
+				File.WriteAllText(path, locationString);
 				//transform.localPosition = new Vector3(100f, 100f, 100f);
 			//}
 		}
