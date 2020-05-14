8fd32a2cb59d42eef718a6d3b631289706275e8b
Justine Batongmalaki <jbt296@yahoo.com>
2020-05-14 10:08:02 -0500

    UI additions
    revised GamePlay and Scoreboard scenes, and created Leaderboard scene and ExitScript

---
+++ Infrared-Arena/Assets/ExitScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitScript : MonoBehaviour
+{
+    //public GameObject helpMenuUI;
+
+    //public static bool aboutOpen = false;
+    //public GameObject aboutUI;
+    //private string openedObject;
+    public GameObject exitMenuUI;
+    public static bool exitOpen = false;
+    private string openedObject;
+
+    private void Update()
+    {
+            if (exitOpen) {
+                exitMenuUI.SetActive(true);
+                exitOpen = true;
+            }
+            else
+            {
+                exitMenuUI.SetActive(false);
+                exitOpen = false;
+            }
+    }
+    public void OnMouseDown(string openBox)
+    {
+        openedObject = openBox;
+        if (openBox == "exit")
+        {
+            exitOpen = true;
+        }
+    }
+
+    public void Close()
+    {
+        if (openedObject == "exit")
+        {
+            exitMenuUI.SetActive(false);
+            exitOpen = false;
+        }
+    }
+}
